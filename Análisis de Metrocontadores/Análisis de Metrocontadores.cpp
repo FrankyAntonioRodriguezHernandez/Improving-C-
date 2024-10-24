1edd939bab043f6aced6e4c95c6268260f8f0895
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-24 00:22:23 -0400

    variables and arrays created and Metrocontador class

--- Análisis de Metrocontadores/Análisis de Metrocontadores.cpp
+++ Análisis de Metrocontadores/Análisis de Metrocontadores.cpp
@@ -5,5 +5,29 @@ using namespace std;
 
 int main()
 {
+    int cantClientes, dias=30;
+    float pagototal=0.0;
+    //creo la variable opcion en -1 para evitar errores
+    int opcion=-1;
+
+    cout<<"Bienvenido al programa de analisis de Metrocontadores"<<endl;
+    cout<<"\n"<<endl;
+
+    cout<<"Digite la cantidad de Clientes"<<endl;
+    cin>>cantClientes;
+    cout<<"\n"<<endl;
+
+    //creo el arreglo
+    float arreglo[cantClientes][30];
+
+    //aqui hago que todas las posiciones del arreglo sean 0
+    for(int i=0;i<cantClientes;i++){
+        for(int j=0;j<dias;j++){
+            arreglo[i][j]=0;
+        }
+    }
+
+
+    
     return 0;
 }
