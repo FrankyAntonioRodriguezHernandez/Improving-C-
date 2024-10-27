17718f5ceea035bcba1a6c64d9c10de7b7fed38a
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-27 11:45:18 -0400

    initializing variables in Cajero Autommatico project

--- Cajero Automático/Cajero Automático.cs
+++ Cajero Automático/Cajero Automático.cs
@@ -3,7 +3,13 @@ using namespace std;
 
 int main(){
 
+    int tarjetas[]={(12345678),(87654321)};
+	int pines[]={(1234),(4321)};
+	int opc=-1,saldo=4000;
 
+	cout<<"BIENVENIDO A SU CAJERO AUTOMATICO"<<endl;
+	cout<<"\n";
+    IT(tarjetas,pines,opc);
 
 
 
