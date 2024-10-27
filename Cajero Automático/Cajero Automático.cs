0aaf15a37ae1f8bd903f4d8fe2a51c6af92dfc7a
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-27 11:46:39 -0400

    Switch of options in Cajero Autommatico project

--- Cajero Automático/Cajero Automático.cs
+++ Cajero Automático/Cajero Automático.cs
@@ -11,7 +11,30 @@ int main(){
 	cout<<"\n";
     IT(tarjetas,pines,opc);
 
+    while(opc!=0){
+	    cout<<"Elija la operacion que desea realizar: "<<endl;
+		cout<<"\n";
+        cout<<"1. Consultar saldo."<<endl;
+        cout<<"2. Extraer dinero de cuenta."<<endl;
+        cout<<"3. Depositar dinero en cuenta."<<endl;
+        cout<<"4. Dinero extraido anualmente."<<endl;
+        cout<<"0. Para salir"<<endl;
+        cin>>opc;
+        switch(opc){
+            case 1:
+                cout<<"El saldo de su tarjeta es de: "<<saldo<<endl;break;
 
+            case 2:
+                saldo = EX(saldo);break;
+            case 3:
+                saldo = ING(saldo, tarjetas,pines, opc);break;
+            case 4:
+                Anno();break;
+
+            case 0:
+                cout<<"Gracias por preferirnos"<<endl;break;
+        }
+    }
 
     return 0;
 }
